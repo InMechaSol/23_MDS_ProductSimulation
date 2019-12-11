457fa0864ada0f958e43c4fb393996d086177e0c
Gary S. Eades <gary.eades@inmechasol.com>
2019-12-10 21:48:58 -0500

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MDS_ProductSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
